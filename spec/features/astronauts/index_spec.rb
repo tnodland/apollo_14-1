41c5272e6a380650a6848bec2000c50a51bf0231
Trevor Nodland <trevor.nodland@gmail.com>
2019-03-22 10:36:37 -0600

    finishes user story 3

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe "astronaut index page" do
     before :each do
       @a1 = Astronaut.create(name: "trevor", age: 24, job: "codenaut")
       @a2 = Astronaut.create(name: "spock", age: 56, job: "logic guy")
+      @m1 = @a1.missions.create(title: "snack run", time_in_space: 100)
+      @m2 = @a1.missions.create(title: "mining the moon for rocks", time_in_space: 200)
+      @m3 = @a2.missions.create(title: "saving the universe", time_in_space: 1000)
+      @m4 = @a2.missions.create(title: "visiting mom", time_in_space: 300)
     end
     it "can see info about astronauts" do
 
@@ -28,6 +32,24 @@ RSpec.describe "astronaut index page" do
 
       within ".average_age" do
         expect(page).to have_content("Average Age: #{Astronaut.average_age}")
+      end
+    end
+
+    it "can see a list of missions for each astronaut" do
+      visit astronauts_path
+
+      within "#astronaut-#{@a1.id}" do
+        within "#missions" do
+          expect(page).to have_content("#{@m1.title},")
+          expect(page).to have_content("#{@m2.title},")
+        end
+      end
+
+      within "#astronaut-#{@a2.id}" do
+        within "#missions" do
+          expect(page).to have_content("#{@m3.title},")
+          expect(page).to have_content("#{@m4.title},")
+        end
       end
     end
   end
